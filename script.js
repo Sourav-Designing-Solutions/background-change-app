15bea3625396c8748a963fd387fc4041880abe50
Sourav-Designing-Solutions <sourav.designingsolutions@gmail.com>
2026-01-08 12:13:58 +0530

    implemented all the required features

---
+++ script.js
@@ -0,0 +1,45 @@
+const selectId = document.getElementById.bind(document);
+const log = console.log;
+const addBtn = selectId("addBtn");
+const minusBtn = selectId("minusBtn");
+const output = selectId("output");
+const input = selectId("input");
+const error = selectId("error");
+
+addBtn.addEventListener("click", () => {
+  let inputVal = parseInt(input.value);
+  let currentVal = parseInt(output.innerText);
+  error.style.display = "none";
+  error.innerText = "";
+  if (inputVal) {
+    currentVal += inputVal;
+    output.innerText = currentVal;
+    input.value = "";
+    return;
+  }
+
+  currentVal += 1;
+  output.innerText = currentVal;
+});
+
+minusBtn.addEventListener("click", () => {
+  let inputVal = parseInt(input.value);
+
+  let currentVal = parseInt(output.innerText);
+
+  if (currentVal <= 0 || (inputVal && currentVal - inputVal < 0)) {
+    error.style.display = "block";
+    error.innerText = "The result cannot be less than 0";
+    return;
+  }
+  error.innerText = "";
+  error.style.display = "none";
+  if (inputVal) {
+    currentVal -= inputVal;
+    output.innerText = currentVal;
+    input.value = "";
+    return;
+  }
+  currentVal -= 1;
+  output.innerText = currentVal;
+});
